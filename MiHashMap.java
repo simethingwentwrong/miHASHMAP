8aa47cf3087e486851ca449cb77adafd5c38d497
Yggdrasil <ryuk1967@gmail.com>
2016-03-07 13:07:57 +0100

    Creado  metodo de get

--- MiHashMap.java
+++ MiHashMap.java
@@ -50,4 +50,20 @@ public class MiHashMap
 
         return valorAntiguo;
     }
+    
+    /**
+     * Devuelve el valor asociado con la clave especificada o -1 en caso de que la clave no exista.
+     */
+    public int get(String clave){
+        int valor = -1;
+        boolean contieneClave = false;
+        for(int index = 0; index < claves.length && !contieneClave; index++ )
+        {
+            if (claves[index] == clave){
+                valor = valores[index];
+                contieneClave = true;
+            }
+        }
+        return valor;
+    }
 }
